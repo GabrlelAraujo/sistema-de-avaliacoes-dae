9c99730a8b3f67a00b0845000078f8f995a4c459
unknown <daeamericana@DAE-5691.DAE.local>
2026-07-16 12:14:22 -0300

    Novo modelo de layout para o dae, primeiramente mudando a parte do login

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
+const os = require('os');
 
 require('./database/db'); // garante que as tabelas existam
 
